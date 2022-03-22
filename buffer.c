380937cea6c4b20be8c2ed29865a46bbbb3ac6aa
Oliver Morris <blueman7700@gmail.com>
2022-03-22 14:30:08 +0000

    changed so the buffer uses an argument instead of reading input

--- buffer.c
+++ buffer.c
@@ -1,32 +1,44 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
 
 #define MAX_CHAR 32
 
-int main() {
+int main(int argc, char *argv[]) {
+
     setuid(0);
     printf("Testing!\n");
 
-    char sentence[MAX_CHAR]; //this take the input correctly.
+    char name[MAX_CHAR]; //this take the input correctly.
 
     // The follow code demonstrates that without the correct checks
     // a buffer overflow attack can be made.
-    printf("\nEnter your Sentence: ");
-    gets(sentence);
-    printf("\nEntered Sentence is: %s", sentence);
+
+    if(argc == 1) {
+        printf("Not Enough Arguments! Please Enter a Name\n");
+    } else if (argc == 2){
+        strcpy(name, argv[1]);
+        printf("\nHello %s!\n", name);
+    } else {
+        printf("Too many arguments!\n");
+    }    
 
     // The follow code demonstrates how the attack can be avoided
     // using the correct checks.
-//    printf("\n\nEnter your Sentence: ");
-//    fgets(sentence, MAX_CHAR, stdin);
-//    unsigned long length = strlen(sentence);
-//    printf("\nLength of string: %lu", length);
-//    if (length > MAX_CHAR)
-//    {
-//        printf("\nThe sentence you entered is too long!");
-//    } else {
-//        printf("\nYour sentence is: %s", sentence);
-//    }
+    // if(argc == 1) {
+    //     printf("Please enter a name!");
+    // } else if (argc == 2){
+    //     if(strlen(argv[1]) < MAX_CHAR) {
+    //         strcpy(name, argv[1]);
+    //         printf("\nEntered Sentence is: %s", name);
+    //     } else {
+    //         printf("Input is too long!");
+    //     }
+    // } else {
+    //     printf("Too many arguments!");
+    // }
 
     return 0;
 }
