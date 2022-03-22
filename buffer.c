b9c57a6887e3a23532361253021471b09b0c6550
mason0109 <56236607+mason0109@users.noreply.github.com>
2022-03-22 13:24:17 +0000

    Buffer Overflow vulnerability program
    Program to demonstrate the buffer overflow vulnerability and the potential patches.

---
+++ buffer.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_CHAR 32
+
+int main() {
+    printf("Testing!\n");
+
+    char sentence[MAX_CHAR]; //this take the input correctly.
+
+    // The follow code demonstrates that without the correct checks
+    // a buffer overflow attack can be made.
+    printf("\nEnter your Sentence: ");
+    gets(sentence);
+    printf("\nEntered Sentence is: %s", sentence);
+
+    // The follow code demonstrates how the attack can be avoided
+    // using the correct checks.
+//    printf("\n\nEnter your Sentence: ");
+//    fgets(sentence, MAX_CHAR, stdin);
+//    unsigned long length = strlen(sentence);
+//    printf("\nLength of string: %lu", length);
+//    if (length > MAX_CHAR)
+//    {
+//        printf("\nThe sentence you entered is too long!");
+//    } else {
+//        printf("\nYour sentence is: %s", sentence);
+//    }
+
+    return 0;
+}
